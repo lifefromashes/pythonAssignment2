598b01b3135c1245b520bed24819e3f3d7585777
Kristin <kskipper.03@gmail.com>
2023-09-16 09:50:25 -0500

    Assignment 5 CYBV 473.

--- CYBV473/skipperK_WK4_script5.py
+++ CYBV473/skipperK_WK4_script5.py
@@ -30,22 +30,28 @@ while True:
         break
 
 # Create a PrettyTable for image information
-tbl = PrettyTable(['Image?', 'Format', 'Type', 'Width', 'Height'])
+tbl = PrettyTable(['Image?', 'File Path', 'File Size', 'Ext', 'Format', 'Width', 'Height', 'Type'])
 
 # Use the os.walk method to walk the path from root to bottom
 for root, dirs, files in os.walk(imageFileDir):
     # Iterate through each file in directory and examine using PIL
     for eachItem in files:
         try:
+            # Get file path
+            fileFullPath = os.path.join(root, eachItem)
+            # Get file extension using split txt to split it by root and extension and returns tuple so can get 2nd item
+            # index[1] from that which would be the file ext
+            fileExt = os.path.splitext(eachItem)[1]
             with Image.open(os.path.join(root, eachItem)) as im:
                 imStatus = 'YES'
                 imFormat = im.format
                 imType = im.mode
                 imWidth = im.width
                 imHeight = im.height
+                fileSize = os.path.getsize(fileFullPath)  # Get file size in bytes
 
                 # Add each to pretty table
-                tbl.add_row([imStatus, imFormat, imType, imWidth, imHeight])
+                tbl.add_row([imStatus, fileFullPath, fileSize, fileExt, imFormat, imWidth, imHeight, imType])
 
         except Exception as err:
             print('Exception occurred: ', str(err))
