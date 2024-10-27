18c08fa0616d7b4b750cae33a67b7d5cbb590a39
Kristin <kskipper.03@gmail.com>
2024-10-27 08:45:39 -0500

    Adv python class stuff

--- CYBV474_ADV_Python/wk9testing.py
+++ CYBV474_ADV_Python/wk9testing.py
@@ -1,23 +1,20 @@
 '''
-Script Purpose: Week 9 Exercise #9
-Script Version: Fall 2024
-Script Author:  Kristin Skipper
+Script Purpose: Practical Exercise #9 Starter Script
+Script Version: Fall 2023
+Script Author:  Professor Hand
 '''
-import nltk
 
 print("\nGender Analysis of Tweets")
 print("Loading ML Libraries ... please wait")
 
 import re
 import pandas as pd
-from nltk import trigrams, NaiveBayesClassifier, classify, pos_tag
+from nltk import trigrams, NaiveBayesClassifier, classify
 from sklearn.model_selection import train_test_split
 
 
-
 def scrubTweet(twt):
-    # Updated to allow mentions and hashtags as females often use a lot of hashtags
-    twt = re.sub(r"[^a-zA-Z#@ ]", " ", twt.lower())
+    twt = re.sub("[^a-zA-Z]", ' ', twt.lower())
     return twt
 
 
@@ -50,37 +47,14 @@ def getFeatures(gender, twt, maleWords, femaleWords):
         that will be included.
     '''
 
+    twtLength = len(twt)
+
     features = {}
-    features['charCount'] = len(twt)
-    # Total number of words in the tweet
-    features['tweetLength'] = len(twt.split())
-    # Find the average word length in the tweet
-    features['avgWordLength'] = sum(len(word) for word in twt.split()) / (features['tweetLength'] + 1)
-    # Calculate the number of words in male tweets and female tweets
-    features['maleWordCount'] = sum(1 for word in twt.split() if word in maleWords)
-    features['femaleWordCount'] = sum(1 for word in twt.split() if word in femaleWords)
-    # Count the number of exclamation and question marks
-    features['exclamationCount'] = twt.count('!')
-    features['questionMarkCount'] = twt.count('?')
-    features['mentionCount'] = twt.count('@')
-    features['hashtagCount'] = twt.count('#')
+    features['tweetLength'] = twtLength
 
-    # If values of female/male words are 0, this 0.1 adds a smoothing value to avoid zero-probability issues
-    # & increase accuracy results; referenced ChatGPT for help when the female/male words kept coming up as 0
-    if features['maleWordCount'] == 0 and features['femaleWordCount'] == 0:
-        features['maleWordCount'] = 0.1
-        features['femaleWordCount'] = 0.1
-
-    #Below used in testing to ensure was printing female/male words
-    # print(f'Male words: {len(maleWords)}, Female words: {len(femaleWords)}')
-    # print('Sample Female Words:', list(femaleWords)[:10])
-
-    # Tag each word with a POS tag to help get better accuracy
-    pos_tags = nltk.pos_tag(twt.split())
-    features['nounCount'] = sum(1 for word, tag in pos_tags if tag.startswith('NN'))
-    features['verbCount'] = sum(1 for word, tag in pos_tags if tag.startswith('VB'))
-    features['adjCount'] = sum(1 for word, tag in pos_tags if tag.startswith('JJ'))
-    features['advCount'] = sum(1 for word, tag in pos_tags if tag.startswith('RB'))
+    '''
+    Your Code Goes Here add more features
+    '''
 
     return features
 
@@ -116,7 +90,6 @@ def main():
 
 if __name__ == '__main__':
     main()
-
 
 
 
